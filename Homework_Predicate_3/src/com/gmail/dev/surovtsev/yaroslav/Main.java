0ee71717996ba312fd2d2e0b00039439b17382fb
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-09-09 15:35:26 +0300

    did homework predicate 3

---
+++ Homework_Predicate_3/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -0,0 +1,25 @@
+package com.gmail.dev.surovtsev.yaroslav;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Umka", 12);
+        Cat cat2 = new Cat("Luska", 5);
+        Cat cat3 = new Cat("Barsic", 8);
+        Cat cat4 = new Cat("Timka", 4);
+        Cat cat5 = new Cat("Kuzia", 2);
+        List<Cat> cats = new ArrayList<>(List.of(cat1, cat2, cat3, cat4, cat5));
+        System.out.println(cats);
+
+        int age = 5;
+        String letter = "C";
+        Predicate<Cat> pr1 = cat -> cat.getAge() < age;
+        Predicate<Cat> pr2 = cat -> cat.getName().substring(0, 1).toUpperCase().compareTo(letter.toUpperCase()) > 0;
+        cats.removeIf(pr1.and(pr2));
+        System.out.println(cats);
+    }
+}
