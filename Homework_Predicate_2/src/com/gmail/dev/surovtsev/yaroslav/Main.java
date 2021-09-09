0dabed8de4097d15c2e9e8e5cfd3c71baa77896e
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-09-09 17:26:40 +0300

    did homework predicate 2

--- Homework_Predicate_2/src/com/gmail/dev/surovtsev/yaroslav/Main.java
+++ Homework_Predicate_2/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -1,3 +1,8 @@
+/*2) Используя реализацию Predicate<String> реализуйте удаление со списка строк,
+ * строки первая буква которых задается отдельным массивом символов.
+ * Например если этот массив содержит ['h','a','t'] то со списка стоит
+ * удалить все строки которые начинаются с этих букв.
+ */
 package com.gmail.dev.surovtsev.yaroslav;
 
 import java.util.ArrayList;
@@ -8,6 +13,7 @@ public class Main {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>(List.of("String", "Double", "Integer", "Float", "BigInteger", "Boolean"));
         System.out.println(list);
+
         Character[] letters = new Character[]{'f', 'd'};
         list.removeIf(Main::removeIfStartsWith);
         list.removeIf(s -> {
