9a94dabd3b4372150548ed12509fd4b3d1892ba5
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-09-12 23:09:28 +0300

    did homework comparator 4

--- Homework_Comparator_4/src/com/gmail/dev/surovtsev/yaroslav/Main.java
+++ Homework_Comparator_4/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -12,34 +12,22 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> list1 = new ArrayList<>(List.of(53, 23, 57, 36, 34, 15, 11, 46, 68));
+        List<Integer> list1 = new ArrayList<>(List.of(23, 57, 36, 34, 15, 11, 53, 46, 68));
         List<Integer> list2 = new ArrayList<>(List.of(36, 34, 15, 46, 68));
         System.out.println(list1.toString());
+        System.out.println(list2.toString());
 
         Comparator<Integer> comp1 = Main::numberComparator;
-        Comparator<Integer> comp2 = Main::notPrimeComparator;
+        Comparator<Integer> comp2 = (a, b) -> Integer.compare(b, a);
         Comparator<Integer> comp3 = comp1.thenComparing(comp2);
 
-        list1.sort(comp3);
+        System.out.println("max1: " + Collections.max(list1, comp3));
+        System.out.println("max2: " + Collections.max(list2, comp3));
+
+        Collections.sort(list1, comp3);
         System.out.println(list1);
-        Integer result1 = Collections.max(list1, comp3);
-        System.out.println("result1: " + result1);
-
+        Collections.sort(list2, comp3);
         System.out.println(list2);
-        list2.sort(comp3);
-        System.out.println(list2);
-        Integer result2 = Collections.max(list2, comp3);
-        System.out.println("result2: " + result2);
-    }
-
-    public static int notPrimeComparator(Integer a, Integer b) {
-        if (!checkPrime(a) && !checkPrime(b)) {
-            return Integer.compare(b, a);
-        }
-        if (checkPrime(a) && checkPrime(b)) {
-            return Integer.compare(a, b);
-        }
-        return 0;
     }
 
     public static int numberComparator(Integer a, Integer b) {
@@ -49,12 +37,15 @@ public class Main {
         if (!checkPrime(a) && checkPrime(b)) {
             return -1;
         }
+        if (checkPrime(a) && checkPrime(b)) {
+            return a - b;
+        }
         return 0;
     }
 
     public static Boolean checkPrime(Integer n) {
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0 && i != 1 && i != n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
