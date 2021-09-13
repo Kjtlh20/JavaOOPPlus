80967847f5da052c7218e3f1da1522b5259ff3d9
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-09-13 22:25:28 +0300

    did homework consumer 1

---
+++ Homework_Consumer_1/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -0,0 +1,22 @@
+package com.gmail.dev.surovtsev.yaroslav;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(List.of(4, 5, 8, 98, 54, 67, 34, 65, 34, 31, 22));
+        System.out.println(list);
+
+        Consumer<Integer> cons = Main::printOddNumber;
+        list.forEach(n -> cons.accept(n));
+    }
+
+    public static void printOddNumber(Integer n) {
+        if (n % 2 != 0) {
+            System.out.println(n);
+        }
+    }
+}
