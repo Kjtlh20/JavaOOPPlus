e55998296fd19c0ec79b6635c0b5f395b3a7e63a
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-09-20 19:58:02 +0300

    did homework stream api part7 1

---
+++ Homework_StreamAPI_Part7_1/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -0,0 +1,14 @@
+package com.gmail.dev.surovtsev.yaroslav;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4);
+        Set<Integer> set = list.stream().collect(HashSet::new, (s, a) -> s.add(a), (s1, s2) -> s1.addAll(s2));
+        System.out.println(set);
+    }
+}
